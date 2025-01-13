c3c02ccf176458282f06aafd5f4dbf9c993c4ef3
boazfr1 <boazfr1@gmail.com>
2025-01-13 16:03:37 +0200

    add the client folder

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -9,9 +9,9 @@ import (
 )
 
 func RouteList(mux *http.ServeMux) {
-    mux.HandleFunc("/v1/health", middleware.Get(health));
-	mux.HandleFunc("/v1/boaz", middleware.Get(boaz));
-	mux.HandleFunc("/v1/sign-up", middleware.Post(user.SignUp))
+    mux.HandleFunc("api/v1/health", middleware.Get(health));
+	mux.HandleFunc("api/v1/boaz", middleware.Get(boaz));
+	mux.HandleFunc("api/v1/sign-up", middleware.Post(user.SignUp))
 }
 
 func health(res http.ResponseWriter, req *http.Request) {
